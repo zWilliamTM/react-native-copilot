8188caf4500ac988e8f0944fbe5a722919bf9dfa
William Martinez <skilltrail@gmail.com>
2022-04-01 13:50:50 -0500

    Edit para MRE

--- src/components/style.js
+++ src/components/style.js
@@ -50,7 +50,7 @@ export default StyleSheet.create({
     borderWidth: 2,
     borderRadius: STEP_NUMBER_RADIUS,
     borderColor: '#FFFFFF',
-    backgroundColor: '#27ae60',
+    backgroundColor: '#b22222',
   },
   stepNumberText: {
     fontSize: 10,
@@ -61,7 +61,7 @@ export default StyleSheet.create({
     padding: 10,
   },
   buttonText: {
-    color: '#27ae60',
+    color: '#b22222',
   },
   bottomBar: {
     marginTop: 10,
